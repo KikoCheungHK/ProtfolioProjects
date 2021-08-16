73bdc2ff96839e930bf4f121bafb6372e431a11c
Kiko <72788974+KikoCheungHK@users.noreply.github.com>
2021-08-16 22:36:43 +0800

    Data Cleansing in SQL

--- DataCleansing_NashvilleHousing.sql
+++ DataCleansing_NashvilleHousing.sql
@@ -6,7 +6,6 @@ Cleaning Data in SQL Queries
 Select *
 From PortfolioProject.dbo.NashvilleHousing
 
-
 --------------------------------------------------------------------------------------------------------------------------
 
 -- Standardize Date Format
